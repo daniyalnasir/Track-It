405590748f85293b1ff88f92805a3b511d49586c
Daniyal <s.daniyal.nasir@gmail.com>
2026-09-24 15:34:01 +0500

    Customizable CardView Added

---
+++ core/design-systems/src/main/java/com/codeshod/design_systems/views/CardView.kt
@@ -0,0 +1,24 @@
+package com.codeshod.design_systems.views
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CardView(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        content()
+    }
+}
